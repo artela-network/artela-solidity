e4b86fb11915a9eb7e14047c73b24fbf4d64de1c
Kamil Śliwak <kamil.sliwak@codepoets.it>
2023-04-12 12:55:58 +0200

    fixup! User-defined literal suffixes: Tests

--- test/libsolidity/semanticTests/literalSuffixes/suffix_in_new.sol
+++
@@ -1,9 +0,0 @@
-function twice(uint x) pure suffix returns (uint) { return x * 2; }
-
-contract C {
-    function run() public pure returns (uint[] memory) {
-        return new uint[](5 twice);
-    }
-}
-// ----
-// run() -> 0x20, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
