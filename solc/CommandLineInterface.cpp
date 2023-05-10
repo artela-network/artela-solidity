veniger/solidity

Remove YulStack::Machine enum.

solc/CommandLineInterface.cpp
@@ -638,7 +638,7 @@ void CommandLineInterface::processInput()
 		serveLSP();
 		break;
 	case InputMode::Assembler:
-		assemble(m_options.assembly.inputLanguage, m_options.assembly.targetMachine);
+		assemble(m_options.assembly.inputLanguage);
 		break;
 	case InputMode::Linker:
 		link();
@@ -1005,7 +1005,7 @@ string CommandLineInterface::objectWithLinkRefsHex(evmasm::LinkerObject const& _
 	return out;
 }
 
-void CommandLineInterface::assemble(yul::YulStack::Language _language, yul::YulStack::Machine _targetMachine)
+void CommandLineInterface::assemble(yul::YulStack::Language _language)
 {
 	solAssert(m_options.input.mode == InputMode::Assembler);
 
@@ -1054,7 +1054,6 @@ void CommandLineInterface::assemble(yul::YulStack::Language _language, yul::YulS
 
 	for (auto const& src: m_fileReader.sourceUnits())
 	{
-		solAssert(_targetMachine == yul::YulStack::Machine::EVM);
 		string machine = "EVM";
 		sout() << endl << "======= " << src.first << " (" << machine << ") =======" << endl;
 
@@ -1069,7 +1068,7 @@ void CommandLineInterface::assemble(yul::YulStack::Language _language, yul::YulS
 		}
 
 		yul::MachineAssemblyObject object;
-		object = stack.assemble(_targetMachine);
+		object = stack.assemble();
 		object.bytecode->link(m_options.linker.libraries);
 
 		if (m_options.compiler.outputs.binary)
@@ -1081,7 +1080,6 @@ void CommandLineInterface::assemble(yul::YulStack::Language _language, yul::YulS
 				serr() << "No binary representation found." << endl;
 		}
 
-		solAssert(_targetMachine == yul::YulStack::Machine::EVM, "");
 		if (m_options.compiler.outputs.asm_)
 		{
 			sout() << endl << "Text representation:" << endl;

solc/CommandLineInterface.h
@@ -92,7 +92,7 @@ private:
 	/// @returns the full object with library placeholder hints in hex.
 	static std::string objectWithLinkRefsHex(evmasm::LinkerObject const& _obj);
 
-	void assemble(yul::YulStack::Language _language, yul::YulStack::Machine _targetMachine);
+	void assemble(yul::YulStack::Language _language);
 
 	void outputCompilationResults();
 

artela-network/artela-solidity

solc/CommandLineInterface.cpp
@@ -638,7 +638,7 @@ void CommandLineInterface::processInput()
 		serveLSP();
 		break;
 	case InputMode::Assembler:
-		assembleYul(m_options.assembly.inputLanguage, m_options.assembly.targetMachine);
+		assembleYul(m_options.assembly.inputLanguage);
 		break;
 	case InputMode::Linker:
 		link();
@@ -1009,7 +1009,7 @@ string CommandLineInterface::objectWithLinkRefsHex(evmasm::LinkerObject const& _
 	return out;
 }
 
-void CommandLineInterface::assembleYul(yul::YulStack::Language _language, yul::YulStack::Machine _targetMachine)
+void CommandLineInterface::assembleYul(yul::YulStack::Language _language)
 {
 	solAssert(m_options.input.mode == InputMode::Assembler);
 
@@ -1058,7 +1058,6 @@ void CommandLineInterface::assembleYul(yul::YulStack::Language _language, yul::Y
 
 	for (auto const& src: m_fileReader.sourceUnits())
 	{
-		solAssert(_targetMachine == yul::YulStack::Machine::EVM);
 		string machine = "EVM";
 		sout() << endl << "======= " << src.first << " (" << machine << ") =======" << endl;
 
@@ -1073,7 +1072,7 @@ void CommandLineInterface::assembleYul(yul::YulStack::Language _language, yul::Y
 		}
 
 		yul::MachineAssemblyObject object;
-		object = stack.assemble(_targetMachine);
+		object = stack.assemble();
 		object.bytecode->link(m_options.linker.libraries);
 
 		if (m_options.compiler.outputs.binary)
@@ -1085,7 +1084,6 @@ void CommandLineInterface::assembleYul(yul::YulStack::Language _language, yul::Y
 				serr() << "No binary representation found." << endl;
 		}
 
-		solAssert(_targetMachine == yul::YulStack::Machine::EVM, "");
 		if (m_options.compiler.outputs.asm_)
 		{
 			sout() << endl << "Text representation:" << endl;

solc/CommandLineInterface.h
@@ -92,7 +92,7 @@ private:
 	/// @returns the full object with library placeholder hints in hex.
 	static std::string objectWithLinkRefsHex(evmasm::LinkerObject const& _obj);
 
-	void assembleYul(yul::YulStack::Language _language, yul::YulStack::Machine _targetMachine);
+	void assembleYul(yul::YulStack::Language _language);
 
 	void outputCompilationResults();
 